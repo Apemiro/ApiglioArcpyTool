a812ea6f8f08087b30b61132c880f3ef6381dab1
Apiglio <apiglioetapemiro@gmail.com>
2022-04-23 15:07:53 +0800

    .

--- src/grid.py
+++ src/grid.py
@@ -44,8 +44,38 @@ def latlong2FID(lng,lat):
 	return str(y).zfill(f_digit)+str(x).zfill(f_digit)
 	
 	
+def current_data_center(data_frame_name):
+	mxd = arcpy.mapping.MapDocument(r"CURRENT")
+	data_frames = arcpy.mapping.ListDataFrames(mxd, data_frame_name)
+	if data_frames==[]:
+		raise Exception("找不到符合条件的数据框")
+	else:
+		data_frame = data_frames[0]
+	ll=data_frame.extent.lowerLeft
+	ur=data_frame.extent.upperRight
+	lower=ll.Y
+	left=ll.X
+	upper=ur.Y
+	right=ur.X
+	return [left+(right-left)/2.0,lower+(upper-lower)/2.0]
 	
 	
+def build_related_grid(x,y,data_name,offset=250):
+	arcpy.CreateFeatureclass_management("in_memory", data_name, "POLYGON")
+	
+	x0=x-offset
+	x1=x+offset
+	y0=y-offset
+	y1=y+offset
+	
+	p1=arcpy.Point(x0,y0)
+	p2=arcpy.Point(x1,y0)
+	p3=arcpy.Point(x1,y1)
+	p4=arcpy.Point(x0,y1)
+	arr = arcpy.Array([p1,p2,p3,p4])
+	polygon = arcpy.Polygon(arr)
+	cursor = arcpy.da.InsertCursor(data_name, ["SHAPE@"])
+	cursor.insertRow([polygon])
 	
 	
 	
