aae75b283ef140605b7077e696346e52dc04321d
Apiglio <553724286@qq.com>
2023-08-09 15:54:51 +0800

    export by feature toolbox

--- src/codetool/doc.py
+++ src/codetool/doc.py
@@ -6,6 +6,18 @@ import os.path
 import math
 from feature import to_list
 
+_export_ext_func_ = {"png":arcpy.mapping.ExportToPNG,
+					 "jpg":arcpy.mapping.ExportToJPEG,
+					 "jpeg":arcpy.mapping.ExportToJPEG,
+					 "tif":arcpy.mapping.ExportToTIFF,
+					 "tiff":arcpy.mapping.ExportToTIFF,
+					 "bmp":arcpy.mapping.ExportToBMP,
+					 "pdf":arcpy.mapping.ExportToPDF,
+					 "svg":arcpy.mapping.ExportToSVG,
+					 "eps":arcpy.mapping.ExportToEPS,
+					 "emf":arcpy.mapping.ExportToEMF,
+					 "ai":arcpy.mapping.ExportToAI,
+					}
 
 def cmxd():
 	return arcpy.mapping.MapDocument("CURRENT")
@@ -14,20 +26,30 @@ def lyrs():
 	return arcpy.mapping.ListLayers(cmxd(),"*")
 	
 # 类似于数据驱动页面，后期增加新功能
-def export_by_features(path,feature,field=None):
+def export_by_features(path,feature,field=None,ext="png",resolution=None):
 	fea=to_list(feature)
 	fcount=len(fea)
-	fields=set(map(lambda x:x.name,arcpy.Describe(feature).fields))
-	if field in fields:
+	fields=arcpy.Describe(feature).fields
+	field_names=[x.name for x in fields]
+	if field in field_names:
+		fieldobj=fields[field_names.index(field)]
 		filename=to_list(feature,field)
+		if fieldobj.type==u'String':
+			pass
+		elif fieldobj.type in [u'Double', u'Single', u'Integer']:
+			filename=[str(x) for x in filename]
+		else:
+			raise Exception('不支持的命名字段类型。')
 	else:
-		filename=list(map(lambda x:str(x).zfill(int(math.ceil(math.log10(fcount+1)))),range(fcount)))
+		digit = str(int(math.ceil(math.log10(fcount+1))))
+		fmt = "%0"+digit+"d"
+		filename=[fmt%x for x in range(fcount)]
 	mxd=cmxd()
 	dfm=cmxd().activeDataFrame
 	if not os.path.exists(path):
 		os.makedirs(path)
 	for ii in range(fcount):
 		dfm.panToExtent(fea[ii].extent)
-		arcpy.mapping.ExportToPNG(mxd,path+'/'+str(filename[ii])+".png")
+		_export_ext_func_[ext.lower()](mxd,path+'/'+filename[ii]+"."+ext,resolution=resolution)
 	
 	
--- tbx/exporter_by_feature.py
+++ tbx/exporter_by_feature.py
@@ -10,7 +10,9 @@ import src.codetool.doc
 path_name=arcpy.GetParameterAsText(0)
 feature_name=arcpy.GetParameterAsText(1)
 field_name=arcpy.GetParameterAsText(2)
+ext=arcpy.GetParameterAsText(3)
+dpi=int(arcpy.GetParameterAsText(4))
 if field_name=="#" or field_name=="":
 	field_name=None
 
-src.codetool.doc.export_by_features(path_name,feature_name,field_name)
+src.codetool.doc.export_by_features(path_name,feature_name,field_name,ext,dpi)
