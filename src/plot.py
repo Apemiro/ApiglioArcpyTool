056f747aaced4a5260a03f42c6a7221de56b3589
Apiglio <553724286@qq.com>
2024-02-08 18:56:26 +0800

    plot.grid修改显示设置; 新增与此相关的tbx

--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,17 @@
 # -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
 import matplotlib.colors as mpc
+import matplotlib.font_manager as ftm
 import math
 import gc
+import os
+p=os.path.split(__file__)[0]
+ch_font = ftm.FontProperties(fname=p+'/../assets/fonts/simhei.ttf')
 
+def __floor(num,frac):
+	return num - num%frac
+def __ceil(num,frac):
+	return num - num%frac + frac
 
 def lines(data, save_filename, xlabel, ylabel, figsize=None, dpi=300):
 	if figsize == None:
@@ -19,8 +27,10 @@ def lines(data, save_filename, xlabel, ylabel, figsize=None, dpi=300):
 	plt.close()
 	gc.collect()
 
-def grids(data, save_filename, xlabel, ylabel, figsize=None, dpi=300, colorsmap=None, cellscale=None):
+def grids(data, save_filename, xlabel, ylabel, figsize=None, dpi=300, colorsmap=None, cellscale=None, key=lambda arr:sum(arr)):
 	'''argument data need list of tuple (x, y)'''
+	
+	#计算数据边界
 	x_min = float('+Inf')
 	x_max = float('-Inf')
 	y_min = x_min
@@ -35,24 +45,37 @@ def grids(data, save_filename, xlabel, ylabel, figsize=None, dpi=300, colorsmap=
 	population = len(data)
 	if cellscale==None:
 		proportion = range_x / float(range_y)
-		cnt_col = int((population * proportion)**0.5)+1
+		cnt_col = int((population)**0.5)+1
 		cnt_row = int((population / proportion)**0.5)+1
 		cellscale = (range_x / cnt_col, range_y / cnt_row)
 	else:
 		cnt_col = int(range_x / cellscale[0])+1
 		cnt_row = int(range_y / cellscale[1])+1
 		proportion = cellscale[0] / float(cellscale[1])
+		
+	reso_x = math.floor(math.log10(cellscale[0]))
+	reso_y = math.floor(math.log10(cellscale[1]))
+	x_min = __floor(x_min, 10**reso_x)
+	y_min = __floor(y_min, 10**reso_y)
+	x_max = __ceil(x_max, 10**reso_x)
+	y_max = __ceil(y_max, 10**reso_y)
 	
+	#统计网格中的数据
 	grid_data = []
 	for row in range(cnt_row):
-		grid_data.append([0 for x in range(cnt_col)])
+		grid_data.append([[] for x in range(cnt_col)])
 	for datum in data:
 		grid_x = int(cnt_col * (datum[0] - x_min) / range_x)
 		grid_y = int(cnt_row * (datum[1] - y_min) / range_y)
 		if grid_x >= cnt_col: grid_x = cnt_col-1
 		if grid_y >= cnt_row: grid_y = cnt_row-1
-		grid_data[grid_y][grid_x] += 1
+		if len(datum)<3:
+			grid_data[grid_y][grid_x].append(1)
+		else:
+			grid_data[grid_y][grid_x].append(datum[2])
+	plot_data = [[key(elem) for elem in row] for row in grid_data]
 	
+	#计算画幅大小
 	if figsize==None:
 		fig_w = cnt_col * 0.5
 		fig_h = cnt_row * 0.5
@@ -61,23 +84,38 @@ def grids(data, save_filename, xlabel, ylabel, figsize=None, dpi=300, colorsmap=
 		if fig_h <  1: fig_h = 1
 		if fig_h > 50: fig_h = 50
 		figsize = (fig_w+2, fig_h)
+	
+	#确定色带位置
+	if figsize[0]>figsize[1]:
+		orientation = 'vertical'
+		pad = 0.05 * figsize[1] / float(figsize[0])
+	else:
+		orientation = 'horizontal'
+		pad = 0.05 * figsize[0] / float(figsize[1])
+
 	fig, axs = plt.subplots(figsize=figsize)
 	if colorsmap == None:
 		colorsmap = mpc.LinearSegmentedColormap.from_list("villband",[(1,1,1),(0.75,0,0)])
-	psm = axs.pcolormesh(grid_data, cmap = colorsmap, shading='nearest')
+	psm = axs.pcolormesh(plot_data, cmap = colorsmap, shading='nearest')
 	axs.grid(True, linestyle='-', color='White', linewidth=3)
-	fig.colorbar(psm, ax=axs)
+	fig.colorbar(psm, ax=axs, orientation=orientation, pad=pad)
 	plt.xlim(0, cnt_col)
 	plt.ylim(0, cnt_row)
-	plt.xlabel(xlabel)
-	plt.ylabel(ylabel)
-	reso_x = -int(math.log10(cellscale[0]))
-	reso_y = -int(math.log10(cellscale[1]))
-	if reso_x<0: reso_x=0
-	if reso_y<0: reso_y=0
-	plt.xticks(range(cnt_col+1), [("%."+str(reso_x)+"f")%(x_min+i*cellscale[0],) for i in range(cnt_col+1)])
-	plt.yticks(range(cnt_row+1), [("%."+str(reso_y)+"f")%(x_min+i*cellscale[1],) for i in range(cnt_row+1)])
-	fig.savefig(save_filename)
+	plt.xlabel(xlabel ,fontproperties=ch_font)
+	plt.ylabel(ylabel ,fontproperties=ch_font)
+	x_ticks=[]
+	x_digit=max(0,int(-reso_x))
+	while len(set(x_ticks))!=cnt_col+1:
+		x_ticks = [("%."+str(x_digit)+"f")%(x_min+i*cellscale[0],) for i in range(cnt_col+1)]
+		x_digit += 1
+	y_ticks=[]
+	y_digit=max(0,int(-reso_y))
+	while len(set(y_ticks))!=cnt_row+1:
+		y_ticks = [("%."+str(y_digit)+"f")%(y_min+i*cellscale[1],) for i in range(cnt_row+1)]
+		y_digit += 1
+	plt.xticks(range(cnt_col+1), x_ticks)
+	plt.yticks(range(cnt_row+1), y_ticks)
+	fig.savefig(save_filename, bbox_inches='tight')
 	fig.clf()
 	plt.close('all')
 	gc.collect()
---
+++ tbx/distanced_landscape_grid.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-
+import os
+import os.path
+p=os.path.split(__file__)[0]
+rp=os.path.realpath(p+"/..")
+import sys
+sys.path.append(rp)
+import src
+import src.specific.skl as skl
+import src.plot as aplot
+import math
+import numpy
+
+def key_count(arr):
+	return(len(arr))
+def key_mean(arr):
+	if len(arr)<2:
+		return(0)
+	return(numpy.mean(arr))
+def key_min(arr):
+	if len(arr)<1:
+		return(0)
+	return(min(arr))
+def key_max(arr):
+	if len(arr)<1:
+		return(0)
+	return(max(arr))
+
+input_filename  = arcpy.GetParameterAsText(0)
+output_filename = arcpy.GetParameterAsText(1)
+value_mode = arcpy.GetParameterAsText(2)
+stats_mode = arcpy.GetParameterAsText(3)
+colorsmap  = arcpy.GetParameterAsText(4)
+
+if value_mode == "COUNT":
+	key = key_count
+else:
+	if stats_mode == "MAX":
+		key = key_max
+	elif stats_mode == "MIN":
+		key = key_min
+	else:
+		key = key_mean
+if colorsmap in [None,""]:
+	colorsmap = "Greens"
+ls = skl.dat_to_landscape(input_filename)
+ls_in_angle_minute = [(x[0]/math.pi*180.0,x[1]/math.pi*180.0,x[2]) for x in ls]
+aplot.grids(ls_in_angle_minute, output_filename, u'\u65b9\u4f4d\u89d2', u'\u4fef\u4ef0\u89d2', cellscale=(1,1), figsize=(200,12), key=key, colorsmap=colorsmap)
